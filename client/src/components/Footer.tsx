c53d6f91283fa8fff67c069448239205dae16e7b
suporteTIM9 <suporte.ti@medianove.com>
2026-08-13 11:46:10 +0100

    Aproxima as colunas do rodapé (largura ajustada ao conteúdo, alinhadas à esquerda)

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -195,7 +195,7 @@ export default function Footer() {
 
       {/* Links grid */}
       <div className="container py-12">
-        <div className="grid grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-[repeat(3,max-content)] gap-8 lg:gap-16">
           {footerLinks.map((col) => (
             <div key={col.heading}>
               <h4

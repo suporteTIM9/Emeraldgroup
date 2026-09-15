1b862159b9136d90062a89637af3f070ec2b472c
suporteTIM9 <suporte.ti@medianove.com>
2026-09-15 16:30:38 +0100

    Navbar: adiciona 'Our Business Clusters' e 'Investors Relations' ao menu desktop

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,7 +8,9 @@ type NavbarProps = { fixed?: boolean };
 const navItems: NavItem[] = [
   { label: "About", href: "/about-us" },
   { label: "Leadership Team", href: "/leadership" },
+  { label: "Our Business Clusters", href: "#clusters" },
   { label: "The Chairman's Talk", href: "/chairmans-talk" },
+  { label: "Investors Relations", href: "/contact" },
   { label: "Contact Us", href: "/contact" },
 ];
 
